9681590b1fddaa95a7bb55f4bb415f7c6f80ab97
Iosif Mihai <iosif.mihaia@gmail.com>
2022-06-29 13:40:11 +0300

    Add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '2484201b4amshef719a419645b79p18c26ejsn799807e9c2d3'
 };
 
 /*
